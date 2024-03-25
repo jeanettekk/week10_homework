b611f42fd183427962e97d815b8395dd1b5784e0
Jeanette <khojeanette@gmail.com>
2024-03-25 16:38:45 +0000

    pet app files

---
+++ pet_app.py
@@ -0,0 +1,41 @@
+from flask import Flask, url_for, render_template
+
+# __name__ lets flask know the root path of the application
+# to locate resources like templates
+app = Flask(__name__)
+
+
+def get_image(name, file_type):
+
+    return url_for('static', filename=f'{name}.{file_type}')
+
+
+@app.route('/')
+def home():
+    return render_template('index_pet.html', title='HOME | Pet Adoption Center', image=get_image)
+
+
+@app.route('/dog/<name>')
+def dog(name):
+
+    return render_template(f'{name}.html', title=f'{name.upper()} | Pet Adoption Center', image=get_image,
+                           name=name.capitalize())
+
+
+@app.route('/cat/<name>')
+def cat(name):
+    return render_template(f'{name}.html', title=f'{name.upper()} | Pet Adoption Center', image=get_image,
+                           name=name.capitalize())
+
+# @app.route('/<animal>')
+# def all_animals(animal)
+#
+#
+#     return render_template()
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+    # app.shutdown()
